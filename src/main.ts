a54f4723f41c2d2c7565ce538fa5975e352bce8f
Danendra Indiarto <indiartodanendra@gmail.com>
2026-05-26 18:20:44 +0700

    tambah swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +14,18 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // SWAGGER
+  const config = new DocumentBuilder()
+    .setTitle('Restaurant API')
+    .setDescription('Backend API Restaurant App')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
