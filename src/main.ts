811e39c03c2e3daf1a4893c2e5450c6dbbec0870
Danendra Indiarto <indiartodanendra@gmail.com>
2026-05-26 18:24:58 +0700

    fix railway

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -14,18 +12,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-
-  // SWAGGER
-  const config = new DocumentBuilder()
-    .setTitle('Restaurant API')
-    .setDescription('Backend API Restaurant App')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-
-  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
