9b528dd4fd7e62bc09ccee2ea735dbee91c58c1f
Danendra Indiarto <indiartodanendra@gmail.com>
2026-05-27 11:24:01 +0700

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,7 +15,18 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Restaurant API')
+    .setDescription('Backend API Restaurant')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 void bootstrap();
